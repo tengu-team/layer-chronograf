a0c71b8736a924ab12b02b9638054c465e40d577
Michiel <michielghyselinck@gmail.com>
2017-12-08 11:14:55 +0100

    kapacitor relation + metadata

--- reactive/layer_chronograf.py
+++ reactive/layer_chronograf.py
@@ -33,7 +33,8 @@ def set_blocked():
 @when('influxdb.available')
 @when_not('layer-chronograf.influxdb-configured')
 def configure_influxdb(influxdb):
-    """Restarts Chronograf with information about InfluxDB instance."""
+    """Relation with InfluxDB has been added. Restarts Chronograf with
+    information about InfluxDB application."""
     DB.set('influxdb_hostname', influxdb.hostname())
     DB.set('influxdb_port', influxdb.port())
     DB.set('influxdb_user', influxdb.user())
@@ -50,6 +51,27 @@ def configure_influxdb(influxdb):
     set_state('layer-chronograf.influxdb-configured')
 
 
+@when('kapacitor.available')
+@when_not('layer-chronograf.kapacitor-configured')
+def configure_kapacitor(kapacitor):
+    """Relation with Kapacitor has been added. Restarts Chronograf with
+    information about Kapacitor application."""
+    DB.set('kapacitor_hostname', kapacitor.host())
+    DB.set('kapacitor_port', kapacitor.port())
+    DB.set('kapacitor_user', kapacitor.username())
+    DB.set('kapacitor_password', kapacitor.password())
+
+    context = {'options': get_options()}
+    render('chronograf.service', '/lib/systemd/system/chronograf.service', context)
+    render('chronograf.service', '/usr/lib/chronograf/scripts/chronograf.service', context)
+    render('init.sh', '/usr/lib/chronograf/scripts/init.sh', context)
+
+    subprocess.check_call(['sudo', 'systemctl', 'daemon-reload'])
+
+    remove_state('layer-chronograf.started')
+    set_state('layer-chronograf.kapacitor-configured')
+
+
 @when('layer-chronograf.installed')
 @when('layer-chronograf.influxdb-configured')
 @when_not('layer-chronograf.started')
@@ -64,7 +86,8 @@ def start_layer_chronograf():
 
 @when('layer-chronograf.influxdb-configured')
 @when_not('influxdb.available')
-def wait_influx_available():
+def unconfigure_influxdb():
+    """When relation with InfluxDB is removed Chronograf has to be stopped."""
     # No relation so set InfluxDB values to None.
     DB.set('influxdb_hostname', None)
     DB.set('influxdb_port', None)
@@ -75,14 +98,42 @@ def wait_influx_available():
     remove_state('layer-chronograf.influxdb-configured')
 
 
-def get_options():
-    """Returns string with options to run Chronograf with."""
-    options = ""
+@when('layer-chronograf.kapacitor-configured')
+@when_not('kapacitor.available')
+def unconfigure_kapacitor():
+    """When relation with Kapacitor is removed Chronograf has to be stopped."""
+    # No relation so set Kapacitor values to None.
+    DB.set('kapacitor_hostname', None)
+    DB.set('kapacitor_port', None)
+    DB.set('kapacitor_user', None)
+    DB.set('kapacitor_password', None)
+    service_stop('chronograf')
+    close_port(8888)
+    remove_state('layer-chronograf.kapacitor-configured')
 
+
+def get_options():
+    """Returns string with options to run Chronograf with. (CLI arguments)"""
+    options = ""
     # If influxdb_hostname is None then that means there is no relation
     # with InfluxDB.
     if DB.get('influxdb_hostname') is not None:
         influxdb_url = 'http://' + DB.get('influxdb_hostname') + ':' + DB.get('influxdb_port')
-        options = '--influxdb-url {} --influxdb-username {} --influxdb-password {}'.format(influxdb_url, DB.get('influxdb_user'), DB.get('influxdb_password'))
+        option = '--influxdb-url {} --influxdb-username {} --influxdb-password {}'.format(influxdb_url, DB.get('influxdb_user'), DB.get('influxdb_password'))
+        options = add_option(options, option)
+    if DB.get('kapacitor_hostname') is not None:
+        kapacitor_url = 'http://' + DB.get('kapacitor_hostname') + ':' + DB.get('kapacitor_port')
+        option = '--kapacitor-url {} --kapacitor-username {} --kapacitor-password {}'.format(kapacitor_url, DB.get('kapacitor_user'), DB.get('kapacitor_password'))
+        options = add_option(options, option)
 
+    return options
+
+
+def add_option(options, option):
+    """Adds an option to a list of options."""
+    if not options:
+        options += option
+    else:
+        options += " "
+        options += option
     return options
