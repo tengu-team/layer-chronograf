ed6e6228781f6e78a6070eb51ee729d40c990fa0
Dixan Peña Peña <dixan.pena@tengu.io>
2018-04-20 16:10:29 +0200

    update charm

--- reactive/layer_chronograf.py
+++ reactive/layer_chronograf.py
@@ -102,14 +102,11 @@ def unconfigure_influxdb():
 @when('layer-chronograf.kapacitor-configured')
 @when_not('kapacitor.available')
 def unconfigure_kapacitor():
-    """When relation with Kapacitor is removed Chronograf has to be stopped."""
     # No relation so set Kapacitor values to None.
     DB.set('kapacitor_hostname', None)
     DB.set('kapacitor_port', None)
     DB.set('kapacitor_user', None)
     DB.set('kapacitor_password', None)
-    service_stop('chronograf')
-    close_port(8888)
     remove_state('layer-chronograf.kapacitor-configured')
 
 
